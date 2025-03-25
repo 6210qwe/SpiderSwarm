4fe99a178c716a0fcc75b9994ac9d91ed91cc17e
张延震 <zhangyanzhen@yutumed.com>
2025-03-25 13:17:47 +0800

    更新版本到 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="bald-spider",
-    version="0.1.1",
+    version="0.1.2",
     author="MindLullaby",
     author_email="3203939025@qq.com",
     description="A distributed spider framework",
